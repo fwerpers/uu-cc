983580ae9c66eb45dba98d7026b47be76d21af7b
Felix Werpers <felix@werpers.com>
2017-07-31 10:08:40 +0200

    Add parser code

--- website/js/main.js
+++ website/js/main.js
@@ -1,13 +1,17 @@
 window.onload = function() {
-    // var app = new Vue({
-    //     el: '#app',
-    //     data: {
-    //         message: 'Hallo'
-    //     }
-    // })
+    var app = new Vue({
+        el: '#vue_test',
+        data: {
+            message: 'You loaded this page on ' + new Date(),
+            visible: true
+        }
+    })
 }
 
 function calculate() {
     var input = document.getElementById('course_input')
-    console.log(input.value)
+    var row = input.value.split('\n')[0]
+    var cell = row.split('\t')[0]
+    console.log(row)
+    console.log(cell);
 }
