02fd41b668804282ed1cd98d795bf41a7116d0fd
Felix Werpers <felix@werpers.com>
2017-07-31 09:04:07 +0200

    Add template for website

---
+++ website/js/main.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    // var app = new Vue({
+    //     el: '#app',
+    //     data: {
+    //         message: 'Hallo'
+    //     }
+    // })
+}
+
+function calculate() {
+    var input = document.getElementById('course_input')
+    console.log(input.value)
+}
