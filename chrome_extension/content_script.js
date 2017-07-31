e2825b076e9beae4082ef04a94d912dd9d8b6ea8
Felix Werpers <felix@werpers.com>
2017-07-31 10:09:54 +0200

    Remove unused code

--- chrome_extension/content_script.js
+++ chrome_extension/content_script.js
@@ -43,25 +43,6 @@ class CourseEntry {
 	}
 }
 
-function getCourseListFromHTML() {
-
-	var entrypoint = document.getElementsByTagName('h3')[0] // "Completed courses" element
-	var endpoint = document.getElementsByTagName('h3')[1] // "Incomplete courses" element
-
-	var container = entrypoint.parentElement.parentElement.parentElement
-	var entries = container.children
-
-	// Create and return a list of course codes
-	var courseList = []
-	var i = 1; // skip header
-	do {
-		courseList.push(entries[i].firstElementChild.innerHTML)
-		i++
-	} while (!entries[i].contains(endpoint))
-
-	return(courseList)
-}
-
 function createResponse() {
 	courseTableModel = new CourseTableModel()
 	courseList = courseTableModel.courseList
