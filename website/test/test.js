70192b6c40801fb0fae905284d30b914d22a3b8e
Felix Werpers <felix@werpers.com>
2017-08-23 14:04:54 +0200

    Add test for credits parsing and fix existing test

--- website/test/test.js
+++ website/test/test.js
@@ -4,12 +4,15 @@ const main = require("../static/js/main.js")
 test('parse incomplete course input', t => {
     var input = "1TD396\tDatoriserad bildanalys I (5 hp)\t \n \tSkriftligt prov\t3 hp\t2015-12-14\t4\t  \t(33,5)\n \tInlämningsuppgifter\t2 hp\t2017-07-04\tG\t  \t "
 
-    var courseTable = new main.CourseTableModel(input)
-    console.log(courseTable);
-    var courseList = courseTable.courseList
-    console.log(courseList[0].credits);
-    t.is(courseList[0].credits, '3 hp')
-    t.pass();
+    var courseList = main.parseCourseResults(input)
+
+    t.is(courseList[0].credits, 3)
+})
+
+test('parse credits string', t => {
+    var creditsString = '3 hp'
+    var credits = main.parseCredits(creditsString)
+    t.is(credits, 3)
 })
 
 test.skip('parse course html', t => {
